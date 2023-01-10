f7d7ba367ee95d7b270e95395fbf3f601797b350
asgarabdulwahid <83691109+asgarabdulwahid@users.noreply.github.com>
2023-01-10 15:48:51 +0800

    Add files via upload

---
+++ Act5/select.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#country').select2();
+});
+
+function myFunction() 
+{
+    if(firstname.value == '' && lastname.value == '')
+    {
+        alert("Please Input Information")
+        }
+         else 
+         {
+           alert("Submitted Successfully")
+          }
+
+}
